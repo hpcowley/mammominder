d59edb7e6b1fac31191789fa51c370e8b224c7ee
Hannah Cowley <hcowley14@gmail.com>
2018-06-19 17:55:11 -0400

    first page of quiz done

--- mammocode/src/App.js
+++ mammocode/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import QuizEnter from './QuizEnter';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Application">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Mammominder</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Quiz">
+          <QuizEnter />
+        </div>
+        <div className="App-footer">
+          <p className="footer-text">
+            For Kim. For Dawn.
+          </p>
+        </div>
       </div>
     );
   }
---
+++ mammocode/src/QuizEnter.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+
+
+export default class QuizEnter extends Component {
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                age: 25,
+                biosex: -1,
+                famhist: -1,
+            }
+        }
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+      };
+
+
+    render () {
+        return (
+        <div>
+            <InputLabel htmlFor="age-native-simple">What is your birthday? </InputLabel>
+            <TextField
+                id="date"
+                type="date"
+                defaultValue="1990-01-01"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={this.handleChange('age')}
+            >
+            </TextField>
+            <br/>
+            <InputLabel>What is your sex? </InputLabel>
+            <NativeSelect
+                value={this.state.biosex}
+                onChange={this.handleChange('biosex')}
+                input={
+                    <Input name="biosex" id="biosex-helper" />
+                }
+            >
+                <option value={-1}/>
+                <option value={0}>Female</option>
+                <option value={1}>Male</option>
+                <option value={2}>Transgender Female</option>
+                <option value={3}>Transgender Male</option>
+            </NativeSelect>
+            <br />
+            <InputLabel>Do you have a family history of breast cancer? </InputLabel>
+            <NativeSelect
+                value={this.state.famhist}
+                onChange={this.handleChange('famhist')}
+                input={
+                    <Input name="famhist" id="famhist-helper" />
+                }
+            >   
+                <option value={-1}/>
+                <option value={0}>Yes</option>
+                <option value={1}>No</option>
+                <option value={2}>I'm not sure</option>
+            </NativeSelect>
+            <br />     
+        </div>
+        )
+
+    }
+}
