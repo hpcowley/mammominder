bd9f9576292479236eb45ec37f972d652bb18d36
Hannah Cowley <hcowley14@gmail.com>
2018-06-19 17:59:07 -0400

    switch to form

--- mammocode/src/QuizEnter.js
+++ mammocode/src/QuizEnter.js
@@ -4,6 +4,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 
 export default class QuizEnter extends Component {
@@ -25,47 +26,50 @@ export default class QuizEnter extends Component {
     render () {
         return (
         <div>
-            <InputLabel htmlFor="age-native-simple">What is your birthday? </InputLabel>
-            <TextField
-                id="date"
-                type="date"
-                defaultValue="1990-01-01"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={this.handleChange('age')}
-            >
-            </TextField>
-            <br/>
-            <InputLabel>What is your sex? </InputLabel>
-            <NativeSelect
-                value={this.state.biosex}
-                onChange={this.handleChange('biosex')}
-                input={
-                    <Input name="biosex" id="biosex-helper" />
-                }
-            >
-                <option value={-1}/>
-                <option value={0}>Female</option>
-                <option value={1}>Male</option>
-                <option value={2}>Transgender Female</option>
-                <option value={3}>Transgender Male</option>
-            </NativeSelect>
-            <br />
-            <InputLabel>Do you have a family history of breast cancer? </InputLabel>
-            <NativeSelect
-                value={this.state.famhist}
-                onChange={this.handleChange('famhist')}
-                input={
-                    <Input name="famhist" id="famhist-helper" />
-                }
-            >   
-                <option value={-1}/>
-                <option value={0}>Yes</option>
-                <option value={1}>No</option>
-                <option value={2}>I'm not sure</option>
-            </NativeSelect>
-            <br />     
+            <form>
+                <InputLabel htmlFor="age-native-simple">What is your birthday? </InputLabel>
+                <TextField
+                    id="date"
+                    type="date"
+                    defaultValue="1990-01-01"
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                    onChange={this.handleChange('age')}
+                >
+                </TextField>
+                <br/>
+                <InputLabel>What is your sex? </InputLabel>
+                <NativeSelect
+                    value={this.state.biosex}
+                    onChange={this.handleChange('biosex')}
+                    input={
+                        <Input name="biosex" id="biosex-helper" />
+                    }
+                >
+                    <option value={-1}/>
+                    <option value={0}>Female</option>
+                    <option value={1}>Male</option>
+                    <option value={2}>Transgender Female</option>
+                    <option value={3}>Transgender Male</option>
+                </NativeSelect>
+                <br />
+                <InputLabel>Do you have a family history of breast cancer? </InputLabel>
+                <NativeSelect
+                    value={this.state.famhist}
+                    onChange={this.handleChange('famhist')}
+                    input={
+                        <Input name="famhist" id="famhist-helper" />
+                    }
+                >   
+                    <option value={-1}/>
+                    <option value={0}>Yes</option>
+                    <option value={1}>No</option>
+                    <option value={2}>I'm not sure</option>
+                </NativeSelect>
+                <br />
+                <Button style={{float: 'right'}}>Next</Button>
+            </form>
         </div>
         )
 
